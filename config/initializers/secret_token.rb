813440ab3997cd6b5bc184854f15051f7fe54955
Zanng <yann.uzel@gmail.com>
2012-09-24 20:09:18 +0200

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+ADEmobil::Application.config.secret_token = '78db66761d7f5afb3fe721160beee27e8790b2d3ee3b157d3e22339d81aec54bccbd89bf0d0738072c9c944ed60bf99c527ac7c556bb46bb2eb9262c565c9621'
